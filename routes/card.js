8eb2e437bb234aa7fa8c4e31ae8d0927b5137b18
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-07 11:33:43 +0500

    transform data on a client

--- routes/card.js
+++ routes/card.js
@@ -1,50 +1,53 @@
-const {Router} = require('express')
+const { Router } = require("express")
 // const Card = require('../models/card')
-const Course = require('../models/course')
+const Course = require("../models/course")
 const router = Router()
 
 function mapCartitems(cart) {
-  return cart.items.map(c => ({
-    ...c.courseId._doc, count: c.count
+  return cart.items.map((c) => ({
+    ...c.courseId._doc,
+    id: c.courseId.id,
+    count: c.count,
   }))
 }
 
 function computePrice(courses) {
   return courses.reduce((total, course) => {
-    return total += course.price*course.count
+    return (total += course.price * course.count)
   }, 0)
 }
 
-router.post('/add', async (req, res) => {
+router.post("/add", async (req, res) => {
   const course = await Course.findById(req.body.id)
   // await Card.add(course)
   await req.user.addToCart(course)
-  res.redirect('/card')
+  res.redirect("/card")
 })
 
-router.delete('/remove/:id', async (req, res) => {
+router.delete("/remove/:id", async (req, res) => {
   // const card = await Card.remove(req.params.id)
   await req.user.removeFromCart(req.params.id)
-  const user = await req.user.populate('cart.items.courseId').execPopulate()
+  const user = await req.user.populate("cart.items.courseId").execPopulate()
 
   const courses = mapCartitems(user.cart)
   const cart = {
-    courses, price: computePrice(courses)
+    courses,
+    price: computePrice(courses),
   }
 
   res.status(200).json(cart)
 })
 
-router.get('/', async (req, res) => {
-  const user = await req.user.populate('cart.items.courseId').execPopulate()
+router.get("/", async (req, res) => {
+  const user = await req.user.populate("cart.items.courseId").execPopulate()
 
   const courses = mapCartitems(user.cart)
 
-  res.render('card', {
-    title: 'Корзина',
+  res.render("card", {
+    title: "Корзина",
     isCard: true,
     courses: courses,
-    price: computePrice(courses)
+    price: computePrice(courses),
   })
 })
 
