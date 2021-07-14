7a1c9566e8da93b8e2c03f147fb9e2e4ae32879b
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-14 10:52:03 +0500

    protection routes

--- routes/card.js
+++ routes/card.js
@@ -1,6 +1,7 @@
 const { Router } = require("express")
 // const Card = require('../models/card')
 const Course = require("../models/course")
+const auth = require('../middleware/auth')
 const router = Router()
 
 function mapCartitems(cart) {
@@ -17,14 +18,14 @@ function computePrice(courses) {
   }, 0)
 }
 
-router.post("/add", async (req, res) => {
+router.post("/add", auth, async (req, res) => {
   const course = await Course.findById(req.body.id)
   // await Card.add(course)
   await req.user.addToCart(course)
   res.redirect("/card")
 })
 
-router.delete("/remove/:id", async (req, res) => {
+router.delete("/remove/:id", auth, async (req, res) => {
   // const card = await Card.remove(req.params.id)
   await req.user.removeFromCart(req.params.id)
   const user = await req.user.populate("cart.items.courseId").execPopulate()
@@ -38,7 +39,7 @@ router.delete("/remove/:id", async (req, res) => {
   res.status(200).json(cart)
 })
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   const user = await req.user.populate("cart.items.courseId").execPopulate()
 
   const courses = mapCartitems(user.cart)
