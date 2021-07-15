ca987d8f0b94d332a161d439c3fc3f72baf3022a
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-15 18:19:54 +0500

    email servise

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://igorg599:Dbhecyzr59@cluster0.vrd8v.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value'
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: ''
 }
