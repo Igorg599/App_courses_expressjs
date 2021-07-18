9ab560a00981f724f376095a13ca7911900f99ef
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-18 21:07:18 +0500

    data improvement

--- utils/validators.js
+++ utils/validators.js
@@ -9,22 +9,27 @@ exports.registerValidators = [
       try {
         const user = await User.findOne({ email: value })
         if (user) {
-          return Promise.reject('Такой email уже занят')
+          return Promise.reject("Такой email уже занят")
         }
       } catch (e) {
         console.log(e)
       }
-    }),
+    })
+    .normalizeEmail(),
   body("password", "Пароль должен быть минимум 6 символов")
     .isLength({ min: 6, max: 56 })
-    .isAlphanumeric(),
-  body("confirm").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("парли должны совпадать")
-    }
-    return true
-  }),
+    .isAlphanumeric()
+    .trim(),
+  body("confirm")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("парли должны совпадать")
+      }
+      return true
+    })
+    .trim(),
   body("name")
     .isLength({ min: 3 })
-    .withMessage("Имя должно быть минимум 3 символа"),
+    .withMessage("Имя должно быть минимум 3 символа")
+    .trim(),
 ]
