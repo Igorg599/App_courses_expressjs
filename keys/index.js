33ec8d62a96e0cc04eb598828db95126b01bf77b
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-20 08:49:03 +0500

    key protection

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://igorg599:Dbhecyzr59@cluster0.vrd8v.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'nodejscourse@email.ru',
-  BASE_URL: 'http://localhost:4001'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
